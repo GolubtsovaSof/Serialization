0db3e3bc526ef98364c0b3052c85842fda3d62c5
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-02-18 01:56:47 +0300

    Добавьте файлы проекта.

---
+++ Serialize/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace Serialize
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(1, 0);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(134, 372);
+			this.listBox1.TabIndex = 0;
+			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 16;
+			this.listBox2.Location = new System.Drawing.Point(156, 0);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(150, 372);
+			this.listBox2.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(332, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(182, 188);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(531, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(257, 22);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(531, 40);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(257, 22);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(531, 68);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(257, 22);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(531, 96);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(257, 22);
+			this.textBox4.TabIndex = 6;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(1, 388);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(134, 23);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Добавить список студентов";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(1, 415);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(134, 23);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Сохранить ";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(156, 388);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(150, 23);
+			this.button3.TabIndex = 9;
+			this.button3.Text = "Добавить студента";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(156, 415);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(150, 23);
+			this.button4.TabIndex = 10;
+			this.button4.Text = "Сохранить";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(460, 293);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(207, 23);
+			this.button5.TabIndex = 11;
+			this.button5.Text = "Изменить информацию";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(460, 322);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(207, 23);
+			this.button6.TabIndex = 12;
+			this.button6.Text = "Сохранить изменения";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.listBox2);
+			this.Controls.Add(this.listBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+	}
+}
+
---
+++ Serialize/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serialize
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            All_lists_of_students all = new All_lists_of_students();
+
+
+        }
+        [Serializable]
+        public class All_lists_of_students
+        {
+            public List_of_students LOF;
+            public All_lists_of_students()
+            { }
+
+            public All_lists_of_students(List_of_students lof)
+            {
+                LOF = lof;
+            }
+        }
+        [Serializable]
+        public class List_of_students
+        {
+            public string Name_of_list;
+            public List<Student> list;
+
+            public List_of_students(string name, List<Student> stud)
+            {
+                Name_of_list = name;
+                list = stud;
+            }
+        }
+        [Serializable]
+        public class Student
+        {
+            public string Name;
+            public int Age;
+            public int Group;
+            public string Img_path;
+
+            public Student()
+            { }
+
+            public Student(string name, int age, int group, string path)
+            {
+                Name = name;
+                Age = age;
+                Group = group;
+                Img_path = path;
+            }
+        }
+    }
+}
